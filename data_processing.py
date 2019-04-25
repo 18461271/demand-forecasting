dd43b7fba9c306801fc783f1aaedc7053d1da2c7
LWang <417689924@qq.com>
2019-04-25 12:51:29 +0200

    Add files via upload

--- data_processing.py
+++ data_processing.py
@@ -17,11 +17,13 @@ from keras import models, layers
 import numpy as np # linear algebra
  # data processing, CSV file I/O (e.g. pd.read_csv)
 from sklearn.preprocessing import MinMaxScaler
+import sys
+from itertools import product, starmap
 
 warnings.filterwarnings("ignore")
-print("python {}".format(sys.version))
+"""print("python {}".format(sys.version))
 print("keras version {}".format(keras.__version__))
-print("tensorflow version {}".format(tf.__version__))
+print("tensorflow version {}".format(tf.__version__))"""
 
 def set_seed(sd=123): #define seed for consistent output
     from numpy.random import seed
@@ -35,24 +37,19 @@ def set_seed(sd=123): #define seed for consistent output
     set_random_seed(sd)
 #set_seed(sd=123)
 import sys
-from itertools import product, starmap
 
-def storeitems():
-    return product(range(1,51), range(1,11))
+def storeitems_column_names(logger_ID):
+    #logger_ID = data.fk_network_meter.unique()
+    return list(map(lambda id : f"logger_{id}_flow"  , logger_ID)) + list(map(lambda id : f"logger_{id}_pressure"  , logger_ID))
 
-
-def storeitems_column_names():
-    return list(starmap(lambda i,s: f'item_{i}_store_{s}_sales', storeitems()))
 
 
 def sales_by_storeitem(df):
     ret = pd.DataFrame(index=df.index.unique())
-    for i, s in storeitems():
-        #print(i,s)
-        #sys.exit()
-
-        ret[f'item_{i}_store_{s}_sales'] = df[(df['item'] == i) & (df['store'] == s)]['sales'].values
-        #print( ret)
+    logger_ID = df.fk_network_meter.unique()
+    for id in logger_ID:
+        ret[f"logger_{id}_flow"] = df[(df["fk_network_meter"]==id)]["flow_value_clean"].values
+        ret[f"logger_{id}_pressure"] = df[(df["fk_network_meter"]==id)]["pressure_value_clean"].values
     return ret
 
 def shift_series(series, days):
@@ -67,8 +64,8 @@ def shift_series_in_df(df, series_names=[], days_delta=90):
     return ret
 
 
-def stack_shifted_sales(df, days_deltas=[1, 90, 360]):
-    names = storeitems_column_names()
+def stack_shifted_sales(df,logger_ID, days_deltas=[1, 90, 360]):
+    names = storeitems_column_names(logger_ID)
     dfs = [df.copy()]
     #print(dfs)
     #sys.exit()
@@ -79,33 +76,33 @@ def stack_shifted_sales(df, days_deltas=[1, 90, 360]):
         dfs.append(shifted)
     return pd.concat(dfs, axis=1, sort=False, copy=False)
 
-def generate_data(df_train, df_test ):
-    df_train.index = pd.to_datetime(df_train['date'])
-    df_train.drop('date', axis=1, inplace=True)
-    df_train = sales_by_storeitem(df_train)
+def generate_data(data):
+    df=data.copy()
+    logger_ID = data.fk_network_meter.unique()
+    df.index = pd.to_datetime(df['timestamp']).sort_values()
+    df.drop('timestamp', axis=1, inplace=True)
+    df = sales_by_storeitem(df)
 
-    df_test.index = pd.to_datetime(df_test['date'])
-    df_test.drop('date', axis=1, inplace=True)
-    df_test['sales'] = np.zeros(df_test.shape[0])
-    df_test = sales_by_storeitem(df_test)
 
-    col_names = list(zip(df_test.columns, df_train.columns))  #('item_1_store_1_sales', 'item_1_store_1_sales')
-    for cn in col_names:
-        assert cn[0] == cn[1]
+    timeofday=(4*df.index.hour + df.index.minute/15 ).astype(int)
+    timeofday_df = pd.get_dummies(timeofday, prefix='timeofday')
+    timeofday_df.index = df.index
 
-    df_test['is_test'] = np.repeat(True, df_test.shape[0])
-    df_train['is_test'] = np.repeat(False, df_train.shape[0])
-    df_total = pd.concat([df_train, df_test])
-    weekday_df = pd.get_dummies(df_total.index.weekday, prefix='weekday')
-    weekday_df.index = df_total.index
-    month_df = pd.get_dummies(df_total.index.month, prefix='month')
-    month_df.index =  df_total.index
-    df_total = pd.concat([weekday_df, month_df, df_total], axis=1)
-    assert df_total.isna().any().any() == False
-    df_total = stack_shifted_sales(df_total, days_deltas=[1])
-    df_total.dropna(inplace=True)
-    sales_cols = [col for col in df_total.columns if '_sales' in col and '_sales_' not in col]
-    stacked_sales_cols = [col for col in df_total.columns if '_sales_' in col]
+    weekday_df = pd.get_dummies(df.index.dayofweek, prefix='dayofweek')
+    weekday_df.index = df.index
+
+    month_df = pd.get_dummies(df.index.month, prefix='month')
+    month_df.index =  df.index
+
+    df_total = pd.concat([timeofday_df, weekday_df, month_df, df], axis=1)
+    #assert df_total.isna().any().any() == False
+    #sys.exit()
+    df_total = stack_shifted_sales(df_total, logger_ID, days_deltas=[1,2,3,4])
+    #df_total.dropna(inplace=True)
+    sales_cols = [col for col in df_total.columns if '_flow' in col and '_flow_' not in col or  '_pressure'  in col and '_pressure_' not in col]
+    #print(sales_cols)
+    #sys.exit()
+    stacked_sales_cols = [col for col in df_total.columns if '_flow_' in col or '_pressure_' in col]
     other_cols = [col for col in df_total.columns if col not in set(sales_cols) and col not in set(stacked_sales_cols)]
 
     sales_cols = sorted(sales_cols)
@@ -113,23 +110,24 @@ def generate_data(df_train, df_test ):
 
     new_cols = other_cols + stacked_sales_cols + sales_cols
     df_total = df_total.reindex(columns=new_cols)
-    assert df_total.isna().any().any() == False
+    #assert df_total.isna().any().any() == False
+
     scaler = MinMaxScaler(feature_range=(0,1))
-    cols_to_scale = [col for col in df_total.columns if 'weekday' not in col and 'month' not in col and "is_test" not in col]
+    cols_to_scale = [col for col in df_total.columns if 'timeofday' not in col and 'dayofweek' not in col and 'month' not in col and "is_test" not in col]
     scaled_cols = scaler.fit_transform(df_total[cols_to_scale])
     df_total[cols_to_scale] = scaled_cols
-    df_train = df_total[df_total['is_test'] == False].drop('is_test', axis=1)
-    df_test = df_total[df_total['is_test'] == True].drop('is_test', axis=1)
+    #df_train = df_total[df_total['is_test'] == False].drop('is_test', axis=1)
 
-    X_cols_stacked = [col for col in df_train.columns if '_past_' in col]
-    X_cols_caldata = [col for col in df_train.columns if 'weekday_' in col or 'month_' in col or 'year' in col]
-    X_cols = X_cols_stacked + X_cols_caldata
+    X_cols_stacked = [col for col in df_total.columns if '_past_' in col]
+    X_cols_caldata = [col for col in df_total.columns if 'timeofday_' in col or 'dayofweek_' in col or 'month_' in col or 'year' in col]
+    X_cols_pressure = [col for col in df_total.columns if  '_pressure'  in col and '_pressure_' not in col]
+    X_cols = X_cols_stacked + X_cols_caldata  + X_cols_pressure
 
-    X = df_train[X_cols]
-    y_cols = [col for col in df_train.columns if col not in X_cols]
-    y = df_train[y_cols]
-    X_test = df_test[X_cols]
-    return X, y, X_test, scaler, df_train, df_test, cols_to_scale, y_cols
+    X = df_total[X_cols]
+    y_cols = [col for col in df_total.columns if col not in X_cols]
+    y = df_total[y_cols]
+
+    return X, y,  scaler,  cols_to_scale, y_cols
 
 
 def ts_data(N,T,D,X):
